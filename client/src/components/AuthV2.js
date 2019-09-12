0c071902f8b82f8ac0e9d6acb0669c1aa841521b
Kody Broussard <kzb801@gmail.com>
2019-09-11 21:19:30 -0500

    updated AuthV2

--- client/src/components/AuthV2.js
+++ client/src/components/AuthV2.js
@@ -68,7 +68,7 @@ export default class AuthV2 extends Component {
                           </a>
                         </div>
                         <div className="container text-lg-center pt-5">
-                          <a href="#" className="btn btn-light btn-lg" onClick={this.logoutAction}>
+                          <a href="/api/auth/logout" className="btn btn-light btn-lg">
                             <div>
                               <span className="svgIcon t-popup-svg">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"/></svg>
@@ -81,17 +81,14 @@ export default class AuthV2 extends Component {
 
     } else {
       return          <div className="container text-lg-center pt-5">
-                        <section>
-                          <Link to ="#" className="btn btn-light btn-lg" onClick={this.loginAction}>
-
-                          <span className="svgIcon t-popup-svg">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"/></svg>
-                         </span>
-                         <span className="button-label"> Sign in with Facebook </span></Link>
-                        </section>
-
-
-
+                          <a href="/api/auth/login/facebook" className="btn btn-light btn-lg">
+                            <div>
+                              <span className="svgIcon t-popup-svg">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"/></svg>
+                              </span>
+                              <span className="button-label"> Sign in with Facebook </span>
+                            </div>
+                          </a>
                       </div>
     }
 
